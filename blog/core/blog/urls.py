700c185bf6b3879e089393fe3fa18b3206e3e56e
MMdrezaw1383 <mmdrezaw2789@gmail.com>
2025-09-14 02:58:29 +0330

    files

--- blog/core/blog/urls.py
+++ blog/core/blog/urls.py
@@ -1,9 +1,22 @@
 from django.urls import path
-from .views import Indexview
+from .views import IndexView,Indexview,RedirectMaktab,PostListView
 from django.views.generic import TemplateView
-
+from django.views.generic.base import RedirectView
+app_name = "blog"
 
 urlpatterns = [
-    path('',Indexview,name= 'fbv_index'),
-    path('cbv-index',TemplateView.as_view(template_name='index.html',extra_context={'name':'ali',}))
+    # path('fbv-index',Indexview,name= 'fbv_index'),
+    
+    # path('cbv-index',TemplateView.as_view(template_name='index.html',extra_context={"name":"ali",}))
+    path('cbv-index/',IndexView.as_view(),name='cbv-index'),
+    # path(
+    # "go-to-index/",
+    # RedirectView.as_view(pattern_name="blog:cbv-index"),
+    # name="go-to-index",)
+    path(
+        'redirect-to-maktab/<int:pk>',RedirectMaktab.as_view(),name="redirectmaktab"
+    ),
+    path(
+        'posts/',PostListView.as_view(),name="postlist"
+    )
 ]
--- blog/core/blog/views.py
+++ blog/core/blog/views.py
@@ -1,13 +1,42 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
+from django.shortcuts import get_object_or_404
+from django.views.generic.list import ListView
+from .models import Post
 # Create your views here.
 
 def Indexview(request):
-    
-    return render(request,'templates/index.html')
+    '''
+    a function based view to show the index page.
+    '''
+    return render(request,'index.html')
 
 class IndexView(TemplateView):
+    '''
+    a class based view to show the index page.
+    '''
+    
     template_name = 'index.html'
 
     def get_context_data(self, **kwargs):
-        return super().get_context_data(**kwargs)
+        context =  super().get_context_data(**kwargs) 
+        context['name'] = 'ali'
+        context['posts'] = Post.objects.all()
+        return context
+    
+class RedirectMaktab(RedirectView):
+    url = 'http://maktabkhooneh.org'
+    # permanent = False
+    # query_string = True
+    def get_redirect_url(self, *args, **kwargs):
+        post = get_object_or_404(Post, pk= kwargs['pk'])
+        print(post)
+        return super().get_redirect_url(*args,**kwargs)
+    
+class PostListView(ListView):
+    model = Post
+    template_name = "index.html"
+
+
+ 
